c8b17b22a8c27be33f4af6c15779bb4542d9496c
vzul <71192680+vzul@users.noreply.github.com>
2024-05-15 12:23:37 +0200

    Update 3. aplicando condiciones.c

--- Control de flujo/3. aplicando condiciones.c
+++ Control de flujo/3. aplicando condiciones.c
@@ -1,1 +1,27 @@
+/******************************************************************************
+
+Leer valor entero y si es 1, escribir UNO, si es 2, escribir 2, si es 3, escribir TRES,
+en otro caso escribir NI UNO, NI DOS, NI TRES 
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int entero;
+    printf("Insertar entero: ");
+    scanf("%d", &entero);
+    
+    if (entero == 1){          // single quotes are for chars!!
+        printf("UNO");
+    } else if (entero == 2) {
+        printf("DOS");
+    } else if (entero == 3) {
+        printf("TRES");
+    } else {
+        printf("NI UNO, NI DOS, NI TRES.");
+    }
+    
+    return 0;
+}
 
